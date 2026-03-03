5e96bbba4c36370d0749ead3fd8eb4b7f4d05e3e
hinnn <hinnn@Hins-MacBook-Pro.local>
2026-03-03 19:01:54 +0800

    Apply the share arg to the application

--- facefusion/uis/layouts/default.py
+++ facefusion/uis/layouts/default.py
@@ -122,4 +122,8 @@ def listen() -> None:
 
 
 def run(ui : gradio.Blocks) -> None:
-	ui.launch(favicon_path = 'facefusion.ico', inbrowser = state_manager.get_item('open_browser'))
+	ui.launch(
+		favicon_path = 'facefusion.ico',
+		inbrowser = state_manager.get_item('open_browser'),
+		share = state_manager.get_item('share')
+	)
